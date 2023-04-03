9aca5f8cf32f5828e88a1c5cec922675612785ac
Tim <13Tim37@gmail.com>
2023-04-04 00:48:18 +0100

    Updated matrix code to use grid

--- matrix.js
+++ matrix.js
@@ -1,69 +1,89 @@
-function clearScreen() {
-  // Clear the console
-  console.clear();
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+// Set the size of the canvas to match the window size
+window.addEventListener('resize', () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Define the size of the characters and the spacing between them
+const charWidth = 24;
+const charHeight = 24;
+const spacing = 5;
+
+// Calculate the number of characters that can fit on a line
+const charsPerLine = Math.floor(canvas.width / (charWidth + spacing));
+
+// Define the grid to hold the characters
+const grid = [];
+
+// Fill the grid with empty spaces
+for (let y = 0; y < canvas.height / (charHeight + spacing); y++) {
+  grid.push([]);
+  for (let x = 0; x < charsPerLine; x++) {
+    grid[y].push('');
+  }
 }
 
-function randomChar() {
-  // Generate a random character from the range of ASCII characters used in the Matrix code
-  const min = 33;
-  const max = 126;
-  const charCode = Math.floor(Math.random() * (max - min + 1)) + min;
-  return String.fromCharCode(charCode);
-}
-
-function matrixCode(canvas) {
-  const ctx = canvas.getContext('2d');
-  const charWidth = 10;
-  const charHeight = 14;
-  const charsPerLine = Math.floor(canvas.width / charWidth);
-  const lines = [];
-
-  function addLine() {
-    const line = [];
-    for (let i = 0; i < charsPerLine; i++) {
-      line.push(randomChar());
+// Define the function to update the grid
+function updateGrid() {
+  // Add new characters to the top row
+  for (let x = 0; x < charsPerLine; x++) {
+    if (Math.random() > 0.98) {
+      const lineLength = Math.floor(Math.random() * (30 - 10 + 1)) + 10;
+      let line = '';
+      for (let i = 0; i < lineLength; i++) {
+        line += String.fromCharCode(Math.floor(Math.random() * (126 - 33 + 1)) + 33);
+      }
+      grid[0][x] = line;
     }
-    lines.push(line);
   }
 
-  function shiftLine(line) {
-    const char = randomChar();
-    line.shift();
-    line.push(char);
+  // Shift all the rows down by one
+  for (let y = grid.length - 1; y >= 0; y--) {
+    for (let x = 0; x < charsPerLine; x++) {
+      if (y === 0) {
+        grid[y][x] = '';
+      } else {
+        grid[y][x] = grid[y - 1][x];
+      }
+    }
   }
+}
 
-  function render() {
-    // Clear the canvas
-    ctx.fillStyle = '#000';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+// Define the function to render the grid
+function renderGrid() {
+  // Clear the canvas
+  ctx.fillStyle = '#000';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    // Render the lines
-    ctx.fillStyle = '#0f0';
-    ctx.font = `${charHeight}px Consolas, monospace`;
-    for (let y = 0; y < lines.length; y++) {
-      const line = lines[y];
-      for (let x = 0; x < line.length; x++) {
-        const char = line[x];
-        const xPos = x * charWidth;
-        const yPos = y * charHeight;
+  // Render the grid
+  ctx.fillStyle = '#0f0';
+  ctx.font = `${charHeight}px Consolas, monospace`;
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < charsPerLine; x++) {
+      const line = grid[y][x];
+      const xPos = x * (charWidth + spacing);
+      const lineLength = line.length;
+      for (let i = 0; i < lineLength; i++) {
+        const char = line.charAt(i);
+        const yPos = (y - (lineLength - i - 1)) * (charHeight + spacing);
         ctx.fillText(char, xPos, yPos);
       }
     }
   }
+}
 
-  // Initialize the lines
-  for (let i = 0; i < canvas.height / charHeight; i++) {
-    addLine();
-  }
-
-  // Continuously shift and render the lines
-  setInterval(() => {
-    for (let i = 0; i < lines.length; i++) {
-      shiftLine(lines[i]);
-    }
-    render();
+// Start the animation loop
+function animate() {
+  updateGrid();
+  renderGrid();
+  setTimeout(() => {
+    requestAnimationFrame(animate);
   }, 50);
 }
 
-// Call the matrixCode function with the desired width and height
-matrixCode(80, 25);
+animate();
