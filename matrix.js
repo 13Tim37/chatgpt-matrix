7ebc65e00410fed575737a69fc381e16b2cd705d
Tim <13Tim37@gmail.com>
2023-04-03 23:33:15 +0100

    Rewrite code to use a canvas

--- matrix.js
+++ matrix.js
@@ -11,20 +11,58 @@ function randomChar() {
   return String.fromCharCode(charCode);
 }
 
-async function matrixCode(width, height) {
-  // Continuously generate and display new frames of Matrix code
-  while (true) {
-    clearScreen();
-    for (let y = 0; y < height; y++) {
-      let line = '';
-      for (let x = 0; x < width; x++) {
-        line += randomChar();
-      }
-      console.log(line);
+function matrixCode(canvas) {
+  const ctx = canvas.getContext('2d');
+  const charWidth = 10;
+  const charHeight = 14;
+  const charsPerLine = Math.floor(canvas.width / charWidth);
+  const lines = [];
+
+  function addLine() {
+    const line = [];
+    for (let i = 0; i < charsPerLine; i++) {
+      line.push(randomChar());
     }
-    // Pause briefly before displaying the next frame
-    await new Promise(resolve => setTimeout(resolve, 100));
+    lines.push(line);
   }
+
+  function shiftLine(line) {
+    const char = randomChar();
+    line.shift();
+    line.push(char);
+  }
+
+  function render() {
+    // Clear the canvas
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Render the lines
+    ctx.fillStyle = '#0f0';
+    ctx.font = `${charHeight}px Consolas, monospace`;
+    for (let y = 0; y < lines.length; y++) {
+      const line = lines[y];
+      for (let x = 0; x < line.length; x++) {
+        const char = line[x];
+        const xPos = x * charWidth;
+        const yPos = y * charHeight;
+        ctx.fillText(char, xPos, yPos);
+      }
+    }
+  }
+
+  // Initialize the lines
+  for (let i = 0; i < canvas.height / charHeight; i++) {
+    addLine();
+  }
+
+  // Continuously shift and render the lines
+  setInterval(() => {
+    for (let i = 0; i < lines.length; i++) {
+      shiftLine(lines[i]);
+    }
+    render();
+  }, 50);
 }
 
 // Call the matrixCode function with the desired width and height
